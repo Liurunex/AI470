4363576d24589d434278e33c94637a5d74bc730b
Liurunex <liurunex@gmail.com>
2017-10-02 15:38:01 -0400

    need dp optimization, ab might not correct

--- Homework2/engines/student.py
+++ Homework2/engines/student.py
@@ -5,7 +5,10 @@ from sys import maxint
 class StudentEngine(Engine):
     """ Game engine that you should you as skeleton code for your 
     implementation. """
-    alpha_beta  = False
+    alpha_beta    = False
+    break_time    = 0
+    cutoffDepth   = 4
+    node_generate = 0
 
     def get_move(self, board, color, move_num=None,
                  time_remaining=None, time_opponent=None):
@@ -23,51 +26,135 @@ class StudentEngine(Engine):
         for move in moves:
             newboard = deepcopy(board)
             newboard.execute_move(move, color)
-            curscore = self.minimax_score(newboard, color*-1, 1)
+            curscore = self.minimax_score(newboard, color*-1, 1, -1)
             if (curscore > res_score):
                 res_score = curscore
                 res_move  = move
+        #print res_move
+        print 'breaktime:', self.break_time
+        print 'node_generate:', self.node_generate
         return res_move
         # Return the best move according to our simple utility function:
         # which move yields the largest different in number of pieces for the
         # given color vs. the opponent?
-        return max(moves, key=lambda move: self._get_cost(board, color, move))
+        #return max(moves, key=lambda move: self._get_cost(board, color, move))
 
-    def minimax_score(self, board, color, curDepth):
-        cutoffDepth = 4
+    def minimax_score(self, board, color, curDepth, isMax):
+        self.node_generate += 1
+        if curDepth == self.cutoffDepth:
+            return self.minimax_evaluation(board, color, isMax)
+
         legalMoves = board.get_legal_moves(color)
-        if curDepth == cutoffDepth or len(legalMoves) == 0:
-            return self.minimax_evaluation(board, color)
-        
-        res_score  = float('inf') if color == 1 else float('-inf')
+        if len(legalMoves) == 0:
+            return self.minimax_evaluation(board, color, isMax)
+
+        res_score  = float('inf') if isMax == -1 else float('-inf')
         for move in legalMoves:
             newboard = deepcopy(board)
             newboard.execute_move(move, color)
-            color_op = color*-1
-            curscore = self.minimax_score(newboard, color_op, curDepth+1)
+            curscore = self.minimax_score(newboard, color*-1, curDepth+1, isMax*-1)
 
-            if (color == 1 and curscore < res_score) or (color == -1 and curscore > res_score):
+            if (isMax == -1 and curscore < res_score) or (isMax == 1 and curscore > res_score):
                 res_score = curscore
-        
+
         return res_score
 
+    def minimax_evaluation(self, board, color, isMax):
+        weight_count     = 0.1
+        weight_lmove     = 1
+        weight_corner    = 10
+        me_color         = color if isMax == 1 else color*-1
+        op_color         = me_color*-1
+        
+        # coin diff
+        coin_score     = board.count(me_color) -  board.count(me_color*-1)
+        
+        # legalMove_diff
+        lmove_op = len(board.get_legal_moves(op_color))
+        lmove_me = len(board.get_legal_moves(me_color))
+        lmove_score = lmove_me - lmove_op
 
-    def minimax_evaluation(self, board, color):
-        num_pieces_op = len(board.get_squares(color*-1))
-        num_pieces_me = len(board.get_squares(color))
+        # corner diff
+        corner_op      = 0
+        corner_me      = 0
+        if board[0][0] == me_color:
+            corner_me += 1
+        if board[0][7] == me_color:
+            corner_me += 1
+        if board[7][7] == me_color:
+            corner_me += 1
+        if board[7][0] == me_color:
+            corner_me += 1
 
-        return num_pieces_me - num_pieces_op
+        if board[0][0] == op_color:
+            corner_op += 1
+        if board[0][7] == op_color:
+            corner_op += 1
+        if board[7][7] == op_color:
+            corner_op += 1
+        if board[7][0] == op_color:
+            corner_op += 1
+        corner_score   = corner_me - corner_op
+       
+        return weight_count*coin_score + weight_lmove*lmove_score + weight_corner*corner_score
 
     def get_ab_minimax_move(self, board, color, move_num=None,
                  time_remaining=None, time_opponent=None):
         """ Skeleton code from greedy.py to get you started. """
         # Get a list of all legal moves.
-        moves = board.get_legal_moves(color)
-        
+        moves     = board.get_legal_moves(color)
+        res_score = float('-inf');
+        alpha     = float('-inf');
+        beta      = float('inf');
+        for move in moves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move, color)
+            curscore = self.ab_minimax_score(newboard, color*-1, 1, -1, alpha, beta)
+            if (curscore > res_score):
+                res_score = curscore
+                res_move  = move
+            """
+            alpha = max(alpha, res_score)
+            if beta <= alpha:
+                break
+            """
+        print 'breaktime:', self.break_time
+        print 'node_generate:', self.node_generate
+        return res_move
         # Return the best move according to our simple utility function:
         # which move yields the largest different in number of pieces for the
         # given color vs. the opponent?
-        return max(moves, key=lambda move: self._get_cost(board, color, move))
+        #return max(moves, key=lambda move: self._get_cost(board, color, move))
+
+    def ab_minimax_score(self, board, color, curDepth, isMax, alpha, beta):
+        self.node_generate += 1
+        if curDepth == self.cutoffDepth:
+            return self.minimax_evaluation(board, color, isMax)
+
+        legalMoves = board.get_legal_moves(color)
+        if len(legalMoves) == 0:
+            return self.minimax_evaluation(board, color, isMax)
+
+        res_score  = float('inf') if isMax == -1 else float('-inf')
+        for move in legalMoves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move, color)
+            curscore = self.ab_minimax_score(newboard, color*-1, curDepth+1, isMax*-1, alpha, beta)
+
+            # alpha_beta core modification
+            if isMax == 1:
+                res_score = max(res_score, curscore)
+                alpha     = max(res_score, alpha)
+            else:
+                res_score = min(res_score, curscore)
+                beta      = min(res_score, beta)
+
+            if beta <= alpha:
+                self.break_time += 1
+                break
+
+        return res_score
+
 
     def _get_cost(self, board, color, move):
         """ Return the difference in number of pieces after the given move 
