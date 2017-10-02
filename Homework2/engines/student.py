4bd4f7d4b2aea17bc7fcacb742a153060d453204
Liurunex <liurunex@gmail.com>
2017-10-02 01:12:49 -0400

    minimax outoftime

--- Homework2/engines/student.py
+++ Homework2/engines/student.py
@@ -1,11 +1,11 @@
 from engines import Engine
 from copy import deepcopy
+from sys import maxint
 
 class StudentEngine(Engine):
     """ Game engine that you should you as skeleton code for your 
     implementation. """
-
-    alpha_beta = False
+    alpha_beta  = False
 
     def get_move(self, board, color, move_num=None,
                  time_remaining=None, time_opponent=None):
@@ -19,11 +19,44 @@ class StudentEngine(Engine):
         """ Skeleton code from greedy.py to get you started. """
         # Get a list of all legal moves.
         moves = board.get_legal_moves(color)
-        
+        res_score = float('-inf');
+        for move in moves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move, color)
+            curscore = self.minimax_score(newboard, color*-1, 1)
+            if (curscore > res_score):
+                res_score = curscore
+                res_move  = move
+        return res_move
         # Return the best move according to our simple utility function:
         # which move yields the largest different in number of pieces for the
         # given color vs. the opponent?
         return max(moves, key=lambda move: self._get_cost(board, color, move))
+
+    def minimax_score(self, board, color, curDepth):
+        cutoffDepth = 4
+        legalMoves = board.get_legal_moves(color)
+        if curDepth == cutoffDepth or len(legalMoves) == 0:
+            return self.minimax_evaluation(board, color)
+        
+        res_score  = float('inf') if color == 1 else float('-inf')
+        for move in legalMoves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move, color)
+            color_op = color*-1
+            curscore = self.minimax_score(newboard, color_op, curDepth+1)
+
+            if (color == 1 and curscore < res_score) or (color == -1 and curscore > res_score):
+                res_score = curscore
+        
+        return res_score
+
+
+    def minimax_evaluation(self, board, color):
+        num_pieces_op = len(board.get_squares(color*-1))
+        num_pieces_me = len(board.get_squares(color))
+
+        return num_pieces_me - num_pieces_op
 
     def get_ab_minimax_move(self, board, color, move_num=None,
                  time_remaining=None, time_opponent=None):
